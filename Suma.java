3a0cfad6edc5a344768597bf091e0e7f9e02f105
Carlos-ufpso <cjperezma@ufpso.edu.co>
2025-03-05 16:28:49 -0500

    (feat): se creo un archivo de java llamado Suma

---
+++ Suma.java
@@ -0,0 +1,10 @@
+public class Suma {
+    public static void main(String[] args) {
+        int numero1 = 5; // Primer número
+        int numero2 = 10; // Segundo número
+
+        int suma = numero1 + numero2; // Sumar los dos números
+
+        System.out.println("La suma de " + numero1 + " y " + numero2 + " es: " + suma);
+    }
+}
